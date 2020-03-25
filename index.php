9f28d883506bbe742fd096dd1699f2c4165c121a
Your Name <frederiksentommy781@gmail.com>
2020-03-25 20:50:05 +0100

    Added some basic HTML

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<style>
+		html, body{
+			font: 100 16px/16px "Gill Sans", "Gill Sans MT", "Myriad Pro", "DejaVu Sans Condensed", Helvetica, Arial, "sans-serif";
+		}
+	</style>
+	<title>tommyf-dk</title>
+</head>
+<body>
+	<?php 
 	echo "php echo'ing";
 ?>
+	<h1>Hello world</h1>
+	
+</body>
+</html>
